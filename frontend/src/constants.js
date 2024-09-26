5bdfae3bbdeac6e2908a414c93369ff14dccecde
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-26 07:52:07 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL = 'https://fullweb-ec1i.onrender.com'; // Render backend URL
 export const PRODUCTS_URL = `${BASE_URL}/api/v1/products`;
-export const USERS_URL = `${BASE_URL}api/v1/users`;
+export const USERS_URL = `${BASE_URL}/api/v1/users`;
 export const ORDERS_URL = `${BASE_URL}/api/v1/orders`;
 export const UPLOAD_URL = `${BASE_URL}/api/v1/upload`;
 export const RAZORPAY_URL = `${BASE_URL}/api/v1/payment`;
