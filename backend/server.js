802e14cd5497cfa3773528f69c10823479155a17
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-26 12:09:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
-const frontendURL = process.env.FRONTEND_URL || 'https://fullweb-ec1i.onrender.com';
+const frontendURL = process.env.FRONTEND_URL || 'https://equipmentool.netlify.app';
 
 connectDB();
 
