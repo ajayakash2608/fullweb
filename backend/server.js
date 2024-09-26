4433ba137b25dca631b36d82aa5aa4eaeceb97d1
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-26 07:22:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,19 +15,23 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
+const frontendURL = process.env.FRONTEND_URL || 'https://main--equipmentool.netlify.app/';
 
-// Connect to MongoDB
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: frontendURL,
+  credentials: true,
+}));
+
 app.use(compression());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const __dirname = path.resolve(); // Set {__dirname} to current working directory
+const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/v1/products', productRoutes);
@@ -35,24 +39,21 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/orders', orderRoutes);
 app.use('/api/v1/upload', uploadRoutes);
 app.use('/api/v1/payment', paymentRoutes);
-//-------------------------------------
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  //any app route that is not api will redirected to index.html
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Backend server running...');
   });
 }
 
-//-------------------------------------
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at ${frontendURL}`);
 });
